e66220c38cdd4ee851b27c84dad4b8f1aae4cde5
William Rivera <william.r.2048@gmail.com>
2023-03-23 22:35:26 -0400

    set up ability to query and view, still needs adjustments to gui and code refactoring

--- testing/GUI/components/viewport_main.py
+++ testing/GUI/components/viewport_main.py
@@ -6,15 +6,47 @@ import os
 
 
 server_connection = None
+cursor = None
 
 def print_me(sender):
     print(f"Menu Item: {sender}")
 
-def connect_to_database(sender, app_data, user_data):
-    print(sender)
-    print(app_data)
-    print(user_data)
-    print(dpg.get_value(sender))
+def collect_debug_values(sender, app_data, user_data):
+    print(f"""
+    Sender: {sender}
+    App Data: {app_data}
+    User Data: {user_data}
+    Sender Values: {dpg.get_value(sender)}
+    """)
+
+
+def use_database(sender, app_data):
+    
+    def populate_tables(connection: MariaDB.connection_cext.CMySQLConnection):
+        cursor = connection.cursor()
+        cursor.execute("SHOW TABLES;")
+        table_names = [table[0] for table in cursor]
+    
+        dpg.configure_item(sql_tables_listbox, items=table_names)
+
+    global server_connection
+    assert type(server_connection) == MariaDB.connection_cext.CMySQLConnection
+    try:
+        global cursor
+        cursor = server_connection.cursor()
+        cursor.execute(f"USE {app_data};")
+        DATABASE_NAME = f"Column Names for: {app_data}"  # TODO will improve later
+        dpg.configure_item(item=sql_db_column_name_portal, default_value=DATABASE_NAME)
+
+        populate_tables(server_connection)
+
+
+    
+    except AssertionError:
+        print(AssertionError)
+
+
+
 
 def login(sender, app_data, user_data):
     login_value = dpg.get_value(login_box)
@@ -36,6 +68,40 @@ def login(sender, app_data, user_data):
 
     except MariaDB.Error as err:
         print(f"{err}")
+
+
+def grab_text(sender, app_data, user_data):  # grabs sender tag, app_data
+
+    def execute_query(sql_cursor: MariaDB.connection_cext.CMySQLConnection, user_data):
+        
+        #assert type(server) == MariaDB.connection_cext.CMySQLConnection
+        try:
+            prompt = dpg.get_value(user_data)
+            sql_cursor.execute(prompt)
+
+            dpg.delete_item('sql-table-view', children_only=True)
+
+            for name in cursor.column_names:
+                dpg.add_table_column(label=name, parent='sql-table-view')    
+
+            for item in cursor:  # how many rows down
+                with dpg.table_row(parent='sql-table-view'):
+                    for data in item:
+                        dpg.add_text(data)
+
+
+        #except AssertionError:
+            #print(AssertionError)
+        except MariaDB.Error as er:
+            print('Error:', er)
+
+
+    if 'sql' in sender:
+        global cursor
+        execute_query(cursor, user_data)  # shuttles to execute if appropriate 
+
+
+
 
 
 def select_one(sender, app_data, user_data):  # select one slider
@@ -60,7 +126,7 @@ def get_databases():  # WHEN USER REFRESHES OR LOGS IN
 
     def append_databases_to_window(cursor):  # nested function, will get name of every server
         db_choices_str = [str(db_choice[0]) for db_choice in cursor]  # TODO rename this lol
-        db_choices = dpg.add_listbox(parent='db-dropdown', num_items=10, width=200, items=db_choices_str, callback=connect_to_database)
+        db_choices = dpg.add_listbox(parent='db-dropdown', num_items=10, width=200, items=db_choices_str, callback=use_database)
         # dpg.add selectable adds selectable straight away, but returns a list of tag ID's
         return db_choices
 
@@ -90,7 +156,7 @@ def get_databases():  # WHEN USER REFRESHES OR LOGS IN
 
 
 dpg.create_context()
-dpg.create_viewport(title='Data Explorer', width=600, height=200)
+dpg.create_viewport(title='Data Explorer', width=900, height=900)
 
 
 
@@ -122,9 +188,31 @@ with dpg.viewport_menu_bar():  # VIEWPORT
 
 
 
+
+
 with dpg.window(label='SQL Query Portal'): # SQL PROMPT
-    dpg.add_text('Column Names')
-    dpg.add_listbox(width=200, tag='column-listbox')
+    sql_db_column_name_portal = dpg.add_text('Column Names:') 
+    with dpg.child_window(height=400, autosize_x=True): 
+        with dpg.group(horizontal=True):
+            sql_tables_listbox = dpg.add_listbox(width=200, tag='column-listbox', num_items=20)  # 1 item is 17.5 px in height
+            dpg.add_input_text(multiline=True, height=350, default_value='SQL Queries go here', tag='sql-box') # make resizeable?
+    
+    with dpg.group(horizontal=True):
+        with dpg.child_window(height=40, width=200):
+            pass
+        with dpg.child_window(height=40, autosize_x=True):
+            dpg.add_button(label='Run Query', callback=grab_text, tag='sql-button', user_data='sql-box')
+
+
+
+    with dpg.child_window():
+        with dpg.group():
+            with dpg.table(header_row=True, policy=dpg.mvTable_SizingFixedFit, row_background=True, reorderable=True,
+                           resizable=True, no_host_extendX=True, hideable=True, borders_innerV=True, delay_search=True,
+                           borders_outerV=True, borders_innerH=True, borders_outerH=True, tag='sql-table-view') as query_results_table:
+                pass
+                
+
 
 
 demo.show_demo()
