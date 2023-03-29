973149da263599b375d20c05fff1a9acfde8573e
William Rivera <william.r.2048@gmail.com>
2023-03-29 00:09:49 -0400

    moved checkbox dropdown to a seperate file, experimenting w/ states

---
+++ testing/GUI/components/checkbox.py
@@ -0,0 +1,69 @@
+import dearpygui.dearpygui as dpg
+
+
+
+def show_checkbox_dropdown(limit: int | bool, choices: list):  # TODO make if False then skip limit
+    with dpg.window(label="Dropdown Selection menu", width =500, height=300):
+        limit = limit
+        activated_list = []
+        capture_ids = []
+
+
+        def limit_checks(sender, app_data):
+
+
+
+            def check_values(app_data: str | int, activated_list: list, limit: int):
+
+                if app_data in activated_list:
+                    activated_list.remove(app_data)
+                elif app_data not in activated_list and len(activated_list) < limit:  # has to be less than b/c 2 < 3, then add 1, then 3 !< 3 : # NOTE can optimize w/ -= and += but kinda buggy rn
+                #elif app_data not in activated_list and limit != 0:    
+                    activated_list.append(app_data)
+
+                return len(activated_list) == limit
+
+
+
+                
+            response = check_values(app_data=app_data, activated_list=activated_list, limit=limit)  # added kwarg style so I dont get confused
+
+
+            if response == True:
+                filter_id_children = dpg.get_item_children('filter_id')
+                for child in filter_id_children[1]:  # dict
+                    child = dpg.get_item_children(child)  # dict of dict
+                    if child[1][1] not in activated_list:
+                        capture_ids.append(child[1])  # list in dict
+                        dpg.configure_item(child[1][1], enabled=False)  # checkbox
+                        dpg.configure_item(child[1][0], color=(255, 0, 0))  # text, red
+            
+
+
+            elif response == False:
+                for id in capture_ids:
+                    dpg.configure_item(id[0], color=(255, 255, 255))  # text, white
+                    dpg.configure_item(id[1], enabled=True)  # re-enable checkboxes
+
+
+
+        with dpg.item_handler_registry(tag='checkbox_handler')as handler:
+            dpg.add_item_activated_handler(callback=limit_checks)
+
+
+        dpg.add_input_text(label="Filter", callback=lambda sender, app_data: dpg.set_value('filter_id', app_data) )  # real time # remember app_data sends info captured 
+        with dpg.child_window(height=200, width=200, tag='window_1', autosize_y=True):  # child window, THEN filter set
+                with dpg.filter_set(tag="filter_id"):  
+                    #test_list = list('ayeee lets get this bread')
+                    # test_list_text_chechbox_id = []
+                    for selection in choices:
+                        # test_list_text_chechbox_id.clear()
+                        dpg.add_group(parent='filter_id', horizontal=True, filter_key=selection)  # this might break if you have identical names..but you prob won't have identical names.
+                        dpg.add_text(default_value=selection, parent=dpg.last_container())
+                        dpg.add_checkbox(parent=dpg.last_container(), user_data=dpg.get_value(dpg.last_item()))  # sender, appdata, user_data
+                        dpg.bind_item_handler_registry(dpg.last_item(), 'checkbox_handler')
+                    
+                    filter_id_children = dpg.get_item_children('filter_id')
+                    for child in filter_id_children[1]:
+                        child = dpg.get_item_children(child)
+                        print(child[1])
--- testing/GUI/components/viewport_main.py
+++ testing/GUI/components/viewport_main.py
@@ -1,6 +1,7 @@
 import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
 import mysql.connector as MariaDB
+from checkbox import show_checkbox_dropdown
 
 import os
 
@@ -234,74 +235,17 @@ with dpg.window(label='Dashboard'):
 
 
 
-# TODO: turn this into its own big function w/ params
-# callback func for checkbox
+cnxpool = MariaDB.pooling.MySQLConnectionPool(pool_name= "mypool", pool_size= 3, database='db_capstone', user=os.getenv('DB_USER'), password=os.getenv('DB_PASSWORD'))
+cnx_1 = cnxpool.get_connection()
+cnx_cur = cnx_1.cursor()
+cnx_cur.execute("""
+SELECT DISTINCT(cust_state)
+FROM cdw_sapp_customer
+ORDER BY 1;
+""")
+states = [state[0] for state in cnx_cur]
 
-with dpg.window(label="Dropdown Selection menu", width =500, height=300):
-    activated_list = []
-    capture_ids = []
-    LIMIT = 3
-
-    def limit_checks(sender, app_data):
-
-
-
-        def check_values(app_data: str | int, activated_list: list, limit: int):
-
-            if app_data in activated_list:
-                activated_list.remove(app_data)
-            elif app_data not in activated_list and len(activated_list) < limit:  # has to be less than b/c 2 < 3, then add 1, then 3 !< 3 : # NOTE can optimize w/ -= and += but kinda buggy rn
-            #elif app_data not in activated_list and limit != 0:    
-                activated_list.append(app_data)
-
-            return len(activated_list) == limit
-
-
-
-            
-        response = check_values(app_data=app_data, activated_list=activated_list, limit=LIMIT)  # added kwarg style so I dont get confused
-
-
-        if response == True:
-            filter_id_children = dpg.get_item_children('filter_id')
-            for child in filter_id_children[1]:
-                child = dpg.get_item_children(child)
-                if child[1][1] not in activated_list:
-                    capture_ids.append(child[1])
-                    dpg.configure_item(child[1][1], enabled=False)
-                    dpg.configure_item(child[1][0], color=(255, 0, 0))
-        
-
-
-        elif response == False:
-            for id in capture_ids:
-                dpg.configure_item(id[0], color=(255, 255, 255))
-                dpg.configure_item(id[1], enabled=True)
-
-
-
-    with dpg.item_handler_registry(tag='checkbox_handler')as handler:
-        dpg.add_item_activated_handler(callback=limit_checks)
-
-
-    dpg.add_input_text(label="Filter (inc, -exc)", callback=lambda sender, app_data: dpg.set_value('filter_id', app_data) )  # real time # remember app_data sends info captured 
-    with dpg.child_window(height=100, width=200, tag='window_1'):  # child window, THEN filter set
-            with dpg.filter_set(tag="filter_id"):  
-                test_list = list('ayeee lets get this bread')
-                test_list_text_chechbox_id = []
-                for selection in test_list:
-                    test_list_text_chechbox_id.clear()
-                    dpg.add_group(parent='filter_id', horizontal=True, filter_key=selection)  # this might break if you have identical names..but you prob won't have identical names.
-                    dpg.add_text(default_value=selection, parent=dpg.last_container())
-                    dpg.add_checkbox(parent=dpg.last_container(), user_data=dpg.get_value(dpg.last_item()))  # sender, appdata, user_data
-                    dpg.bind_item_handler_registry(dpg.last_item(), 'checkbox_handler')
-                
-                filter_id_children = dpg.get_item_children('filter_id')
-                for child in filter_id_children[1]:
-                    child = dpg.get_item_children(child)
-                    print(child[1])
-
-
+show_checkbox_dropdown(3, states)
 
 
 # def change_text(sender, app_data):
