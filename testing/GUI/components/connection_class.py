9c52209cc6bbf2b7d7fa24dcf530addbdcb28a51
William Rivera <william.r.2048@gmail.com>
2023-03-31 16:17:56 -0400

    completed and refined class

--- testing/GUI/components/connection_class.py
+++ testing/GUI/components/connection_class.py
@@ -1,37 +1,7 @@
 import dearpygui.dearpygui as dpg
 import mysql.connector as MariaDB
 from mysql.connector.pooling import PooledMySQLConnection
-from os import getenv
 MariaDB.pooling.MySQLConnectionPool
-from mysql.connector.cursor import MySQLCursor
-
-# test_con = ConnectionHandler(pool_name='TestPool', database='db_capstone', user=getenv('DB_USER'), password=getenv('DB_PASSWORD'), host='localhost')
-
-# cnx_1 = test_con.get_connection()
-
-# cnx_cur = cnx_1.cursor()
-
-# cnx_cur.execute("""
-# SELECT DISTINCT(cust_state)
-# FROM cdw_sapp_customer
-# ORDER BY 1;
-# """)
-
-# for _ in cnx_cur:
-#     print(_)
-
-
-# class ConnectionHandler:
-#     # explicitly define connection pool to instantiate multiple connections
-#     # mothership
-#     def __init__(self,
-#                  pool_name: Optional[str] = None,
-#                  database: Optional[str] = None,
-#                  user: Optional[str] = None,
-#                  password: Optional[str] = None,
-#                  host: str = 'localhost'):
-#         self.__internal_connection = MariaDB.pooling.MySQLConnectionPool(pool_name)
-
 
 
 class ConnectionHandler:
@@ -55,16 +25,20 @@ class ConnectionHandler:
         self.__pool = connection
         self.__connections = {}
         self.__cursors = {}
-        self.returns = {}
+        self.__returns = {}
 
     # assign single connector from the internal connection 
     # connectionhandler.connection1 = connect()
     # single ship 
     # one connection per interface
+
+    def __getitem__(self, connection_name):  # way to access item via dict or iteration
+        return self.__returns[connection_name]
+
     def create_connection(self, connection_name: str):
         """
         Establish a connection manually. 
-        Technically not needed, you can jump right into ConnectionHandler.cur('connection_name')
+        Technically not needed, you can jump right into `ConnectionHandler.cur('connection_name')`
         and a connection will be made for you if none exist.
         """
         self.__connections[connection_name] = self.__pool.get_connection()
@@ -77,6 +51,7 @@ class ConnectionHandler:
         try:
             if type(self.__connections[connection_name]) is PooledMySQLConnection:
                 self.__cursors[connection_name] = self.__connections[connection_name].cursor()
+                self.__returns[connection_name] = {}
                 
         except KeyError:
             self.create_connection(connection_name=connection_name)
@@ -86,22 +61,43 @@ class ConnectionHandler:
     def cur_execute(self, connection_name: str, connection_prompt: str, save: bool = True):
         """
         Executing a cursor with save default on true will allow you to access stored data from the 
-        dictionary at a later time. 
+        dictionary at a later time, or any time you want.
+
+        Execting a cursor with save default on false will allow you to access data only once, and then
+        the data is wiped.
+
         Example: 
             Set up:
-                query = 'select * from table' \n
-                connection = ConnectionHandler() \n
-                connection.cursor('tomato') \n
-                connection.execute('tomato', query) \n
+                `query = 'select * from table'` \n
+                `connection = ConnectionHandler()` \n
+                `connection.cursor('tomato')` \n
+                `connection.execute('tomato', query)` \n
 
-            Access stored data:
-                results = connection.return['tomato'][query]
+            Save is True:
+                Access stored data:
+                    `results = connection['tomato'][query]` \n
+                    can be accessed any time 
+            
+            Save is False: 
+                Access data:
+                    `results = connection.execute('tomato', query)`
+                     must be stored in results or another variable,
+                     or it's lost forever.
+                     can be useful for functions.
+
         """
+        # { 'connection': {query: [results]}, {query2 : [results]},
+        # {'connection2}
+        # }
 
         try:
             self.__cursors[connection_name].execute(connection_prompt)
             if save == True:
-                self.returns[connection_name] = {connection_prompt: [row for row in self.__cursors[connection_name]]}
+                # self.__returns[connection_name] = {connection_prompt: [row for row in self.__cursors[connection_name]]}
+                self.__returns[connection_name].update({connection_prompt: [row for row in self.__cursors[connection_name]]})
+
+
+            
             elif save == False:
                 return [row for row in self.__cursors[connection_name]]
 
@@ -110,7 +106,7 @@ class ConnectionHandler:
             print(f'Error, Cusor Not Found. {e}')
 
 
-    def dict(self, connection_name: str = None, query: str = None):
+    def show(self, connection_name: str = None, query: str = None):
         """
         Dictionary prints the names of the connections and cursors,
         as well as all of the stored queries and values. 
@@ -126,102 +122,79 @@ class ConnectionHandler:
         """ 
         print(self.__connections, self.__cursors)
         if connection_name is None:
-            for key in self.returns.keys():
-                for subkey in self.returns[key].keys():
+            for key in self.__returns.keys():
+                for subkey in self.__returns[key].keys():
                     print(subkey)
-                    internal_list = self.returns[key][subkey]
+                    internal_list = self.__returns[key][subkey]
                     for item in internal_list:
                         print(item)
         elif connection_name is not None and query is None:
-            dictionary = self.returns[connection_name]
+            dictionary = self.__returns[connection_name]
             for query_ in dictionary.keys():
                 print(query_)
                 for row in dictionary[query_]:
                     print(row)
         elif connection_name and query is not None:
-            dictionary = self.returns[connection_name][query]
+            dictionary = self.__returns[connection_name][query]
             print(query)
             for row in dictionary:
                 print(row)
+    
+    def close_connection(self, connection_name: str = None):
+        if connection_name is None:
+
+            for connection in self.__connections.keys():
+                self.__connections[connection].close()
+                print(f"Closed {connection} Successfully")
+                print(f"Closed all Connections in {self.__class__.__name__} sucessfully")
+
+
+        elif connection_name is not None:
+            try:
+                self.__connections[connection_name].close()
+                print(f"Closed {connection_name} succesfully")
+            except KeyError:
+                print(f"Connection Name: {connection_name} not found")
 
 
 
 
 
-query = """
-SELECT * 
-FROM cdw_sapp_customer
-LIMIT 10;
-"""
 
-query_2 = """
-SELECT * 
-FROM cdw_sapp_customer
-LIMIT 2;
-"""
+if __name__ =='__main__':
+    # Examples for Inquiring Minds:
 
+    query = """
+    SELECT * 
+    FROM cdw_sapp_customer
+    LIMIT 10;
+    """
 
-connection = ConnectionHandler(
-    MariaDB.pooling.MySQLConnectionPool(
-    pool_name = 'test',
-    host='localhost',  
-    user=getenv('DB_USER'),
-    password=getenv('DB_PASSWORD'),
-    database='db_capstone'  # db optional
+    query_2 = """
+    SELECT * 
+    FROM cdw_sapp_customer
+    LIMIT 2;
+    """
+
+    from os import getenv
+    connection = ConnectionHandler(
+        MariaDB.pooling.MySQLConnectionPool(
+        pool_name = 'test',
+        host='localhost',  
+        user=getenv('DB_USER'),
+        password=getenv('DB_PASSWORD'),
+        database='db_capstone'  # db optional
+        )
     )
-)
-
-# connection.create_connection('tomato')
-connection.cur('tomato')
-connection.cur_execute('tomato', query)
-connection.cur_execute('tomato', query_2)
-
-
-connection.dict('tomato')
 
 
 
 
-# test = connection.cur_execute('tomato', query_2, save=False)
-
-# print(test)
-
-#server_connection.connection_1
-
-
-####################
-# server_connection = MariaDB.pooling.MySQLConnectionPool(
-#     pool_name = 'test',
-#     host='localhost',  
-#     user=getenv('DB_USER'),
-#     password=getenv('DB_PASSWORD'),
-#     database='db_capstone'  # db optional
-#     )
+    connection.create_connection('tomato')
+    connection.cur('tomato')
+    connection.cur_execute('tomato', query)
+    connection.cur_execute('tomato', query_2)
 
 
-# cnx_cur = server_connection.get_connection()
-# cnx_cur2 = server_connection.get_connection()
-
-# cnx_cur_c = cnx_cur.cursor()
-# cnx_cur2_c = cnx_cur2.cursor()
-
-
-# cnx_cur_c.execute("""
-# SELECT DISTINCT(cust_state)
-# FROM cdw_sapp_customer
-# ORDER BY 1;
-# """)
-
-# cnx_cur2_c.execute("""
-# SELECT * 
-# FROM cdw_sapp_customer
-# LIMIT 10;
-# """)
-
-# for _ in cnx_cur_c:
-#     print(_)
-
-# for _ in cnx_cur2_c:
-#     print(_)
-############################
-
+    connection.show('tomato')
+    connection.close_connection()
